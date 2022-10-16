8bab6b6bbf7a34a2f9740ada51153cc9b2460268
lucasdmmc <lucas.dmmc@hotmail.com>
2022-10-15 21:33:18 -0300

    Configurei as fontes da aplicação no styled components para usar como componente

---
+++ modulo02/desafio-02/coffee-delivery/src/pages/Home/components/Intro/index.tsx
@@ -0,0 +1,16 @@
+import { IntroContainer, IntroContent } from "./styles";
+import introImg from "../../../../assets/intro-img.png"
+
+export function Intro() {
+  return (
+    <IntroContainer>
+      <IntroContent className="container">
+        <div>
+
+        </div>
+
+        <img src={introImg} />
+      </IntroContent>
+    </IntroContainer>
+  )
+}
