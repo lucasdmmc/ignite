b75849cb94943a0dfc9502ee6d7630a351ffb3b9
lucasdmmc <lucas.dmmc@hotmail.com>
2022-10-15 22:57:49 -0300

    Finalizei o content

--- modulo02/desafio-02/coffee-delivery/src/pages/Home/components/Intro/index.tsx
+++ modulo02/desafio-02/coffee-delivery/src/pages/Home/components/Intro/index.tsx
@@ -1,8 +1,12 @@
-import { IntroContainer, IntroContent, IntroText } from "./styles";
+import { BenefitsContainer, IntroContainer, IntroContent, IntroText } from "./styles";
 import introImg from "../../../../assets/intro-img.png"
 import { RegularText } from "../../../../components/Typography";
+import { InfoWithIcon } from "../../../../components/InfoWithIcon";
+import { ShoppingCart, Package, Timer, Coffee } from "phosphor-react"
+import { useTheme } from "styled-components";
 
 export function Intro() {
+  const { colors } = useTheme()
   return (
     <IntroContainer>
       <IntroContent className="container">
@@ -16,6 +20,29 @@ export function Intro() {
               <br></br>qualquer hora
             </RegularText>
           </section>
+
+            <BenefitsContainer>
+              <InfoWithIcon
+                iconBg={colors["brand-yellow-dark"]}
+                icon={<ShoppingCart weight="fill" />}
+                text="Compra simples e segura" 
+              />
+              <InfoWithIcon
+                iconBg={colors["base-text"]}
+                icon={<Package weight="fill" />}
+                text="Embalagem mantém o café intacto" 
+              />
+              <InfoWithIcon
+                iconBg={colors["brand-yellow"]}
+                icon={<Timer weight="fill" />}
+                text="Entrega rápida e rastreada" 
+              />
+              <InfoWithIcon
+                iconBg={colors["brand-purple"]}
+                icon={<Coffee weight="fill" />}
+                text="O café chega frequinho até você" 
+              />
+            </BenefitsContainer>
         </div>
 
         <img src={introImg} />
--- modulo02/desafio-02/coffee-delivery/src/pages/Home/components/Intro/styles.ts
+++ modulo02/desafio-02/coffee-delivery/src/pages/Home/components/Intro/styles.ts
@@ -22,10 +22,18 @@ export const IntroContainer = styled.section`
 export const IntroContent = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   gap: 3.5rem;
 `
 
 export const IntroText = styled(TitleText)`
   margin-bottom: 1rem;
+`
+
+export const BenefitsContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  row-gap: 1.25rem;
+  margin-top: 4.125rem;
 `
