0d9555ef7b4e24136e6a1b019567e8e160036689
lucasdmmc <lucas.dmmc@hotmail.com>
2022-10-19 22:23:46 -0300

    Usei a lib zod e lib react hook form para validação do formulário

--- modulo02/desafio-02/coffee-delivery/src/pages/CompleteOrder/components/CompleteOderForm/AdressForm.tsx
+++ modulo02/desafio-02/coffee-delivery/src/pages/CompleteOrder/components/CompleteOderForm/AdressForm.tsx
@@ -1,16 +1,66 @@
+import { useFormContext } from "react-hook-form";
 import { Input } from "../../../../components/Input";
 import { AdressFormContainer } from "./styles";
+interface ErrosType {
+  errors: {
+    [key: string]: {
+      message: string;
+    };
+  };
+}
 
 export function AdressForm() {
+  const { register, formState } = useFormContext()
+  const { errors } = formState as unknown as ErrosType;
   return(
     <AdressFormContainer>
-      <Input placeholder="CEP" type="number" className="cep"/>
-      <Input placeholder="Rua" className="street"/>
-      <Input placeholder="Número" type="number"/>
-      <Input placeholder="Complemento" className="complement"/>
-      <Input placeholder="Bairro"/>
-      <Input placeholder="Cidade"/>
-      <Input placeholder="UF"/>
+      <Input 
+        placeholder="CEP" 
+        type="number" 
+        className="cep" 
+        {...register("cep")} 
+        error={errors.cep?.message}     
+      />
+
+      <Input 
+        placeholder="Rua" 
+        className="street" 
+        {...register("street")} 
+        error={errors.street?.message}
+      />
+
+      <Input 
+        placeholder="Número" 
+        type="number" 
+        {...register("number")}
+        error={errors.number?.message}
+      />
+
+      <Input
+        placeholder="Complemento"
+        className="complement"
+        {...register("complement")}
+        rightText="Opcional"
+        error={errors.complement?.message}
+      />
+
+      <Input
+        placeholder="Bairro"
+        {...register("distric")}
+        error={errors.distric?.message}
+      />
+
+      <Input
+        placeholder="Cidade"
+        {...register("city")}
+        error={errors.city?.message}
+      />
+
+      <Input 
+        placeholder="UF" 
+        {...register("uf")}
+        error={errors.uf?.message}
+      />
     </AdressFormContainer>
   )
 }
--- modulo02/desafio-02/coffee-delivery/src/pages/CompleteOrder/index.tsx
+++ modulo02/desafio-02/coffee-delivery/src/pages/CompleteOrder/index.tsx
@@ -1,12 +1,43 @@
 import { CompleteOrderForm } from "./components/CompleteOderForm";
 import { SelectedCoffees } from "./components/SelectedCoffees";
 import { CompletedOrderContainer } from "./styles";
+import * as zod from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm, FormProvider } from "react-hook-form";
+
+const confirmOrderFormValidationSchema = zod.object({
+  cep: zod.string().min(1, "Informe o CEP"),
+  street: zod.string().min(1, "Informe a rua"),
+  number: zod.string().min(1, "Informe o número"),
+  complement: zod.string(),
+  distric: zod.string().min(1, "Informe o bairro"),
+  city: zod.string().min(1, "Informe a cidade"),
+  uf: zod.string().min(1, "Informe a UF")
+})
+
+export type OrderData = zod.infer<typeof confirmOrderFormValidationSchema>
+
+type ConfirmOrderFormData = OrderData
 
 export function CompleteOrderPage() {
+  const confirmOrderForm = useForm<ConfirmOrderFormData>({
+    resolver: zodResolver(confirmOrderFormValidationSchema)
+  })
+
+  const { handleSubmit } = confirmOrderForm
+
+  function handleConfirmOrder(data: ConfirmOrderFormData) {
+    console.log(data)
+  }
+
   return (
-    <CompletedOrderContainer className="container">
-      <CompleteOrderForm />
-      <SelectedCoffees />
-    </CompletedOrderContainer>
+    <FormProvider {...confirmOrderForm}>
+      <CompletedOrderContainer 
+        className="container" 
+        onSubmit={handleSubmit(handleConfirmOrder)}>
+        <CompleteOrderForm />
+        <SelectedCoffees />
+      </CompletedOrderContainer>
+    </FormProvider>
   )
 }
