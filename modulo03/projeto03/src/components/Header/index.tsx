867b1da34fd91edb6d35cbc6a6e1975d5aef6dd7
lucasdmmc <lucas.dmmc@hotmail.com>
2022-10-22 20:25:43 -0300

    Interface do projeto finalizado (sem javascropt)

--- modulo03/projeto03/src/components/Header/index.tsx
+++ modulo03/projeto03/src/components/Header/index.tsx
@@ -1,5 +1,9 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/logo.svg"
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
+
+
 export function Header() {
   return (
     <div>
@@ -7,7 +11,13 @@ export function Header() {
         <HeaderContent>
           <img src={logoImg} alt="" />
 
-          <NewTransactionButton>Nova transação</NewTransactionButton>
+          <Dialog.Root>
+            <Dialog.Trigger asChild> 
+              <NewTransactionButton>Nova transação</NewTransactionButton>
+            </Dialog.Trigger>
+
+            <NewTransactionModal />
+          </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
     </div>
